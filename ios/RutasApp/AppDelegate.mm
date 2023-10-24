66c089bb9229fa787b832abb362fca5d2dd8858c
Agu <alemosmusi@gmail.com>
2023-10-24 20:19:34 -0300

    agregado mapa de google

--- ios/RutasApp/AppDelegate.mm
+++ ios/RutasApp/AppDelegate.mm
@@ -1,11 +1,18 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+
+
+  [GMSServices provideAPIKey:@"AIzaSyCjGAcv-k70UBhauN7V-fUYgPLozrnLdVY"]; // add this line using the api key obtained from Google Console
+
   self.moduleName = @"RutasApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 export const MapScreen = () => {
   return (
-    <View>
-        <Text>Map screen</Text>
+    <View style={{flex:1}}>
+        <MapView
+          style={{flex:1}}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
     </View>
   )
 }
